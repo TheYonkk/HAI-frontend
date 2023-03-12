1f7fb98ca0f1c4d11360983b12bd9095a02d078f
Dave Yonkers <dyonkers@prattmiller.com>
2023-03-12 17:43:23 -0400

    Finish(?) hand component, link with backend, and update example page for testing

--- components/handtracker.js
+++ components/handtracker.js
@@ -1,29 +1,202 @@
+import { useEffect, useRef, useState } from "react";
+import { Results, Hands, HAND_CONNECTIONS, VERSION } from "@mediapipe/hands";
+import {
+  drawConnectors,
+  drawLandmarks,
+  Data,
+  lerp,
+} from "@mediapipe/drawing_utils";
+import styles from "./handtracker.module.scss";
 
-import React from 'react';
+/**
+ * A React component that uses MediaPipe to track the user's hands and draw the hand landmarks and connections on a canvas element.
+ * The component also sends the hand data to an API endpoint.
+ * @param {integer} canvasWidth The width of the canvas elementin pixels (video + overlay)
+ * @param {integer} canvasHeight The height of the canvas element in pixels (video + overlay)
+ * @param {string} apiEndpoint The endpoint of the API that will be used to send the hand data to
+ * @returns 
+ */
+export default function HandTracker({canvasWidth, canvasHeight, apiEndpoint}) {
+  const [inputVideoReady, setInputVideoReady] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-const API_ENDPOINT = 'https://ec64-2601-400-c100-3c20-f4d9-cdc5-7e99-6d51.ngrok.io'
+  const inputVideoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
 
-export default function HandTracker() {
+  useEffect(() => {
+    // wait until video is loaded
+    if (!inputVideoReady) {
+      return;
+    }
 
-  const onButtonPress = () => {
-    const fetchData = async () => {
-      const res = await fetch(API_ENDPOINT,{
-        method: 'POST',
+    // both video element and canvas elements are loaded (note that webcam is not loaded yet)
+    if (inputVideoRef.current && canvasRef.current) {
+      // set up canvas
+      contextRef.current = canvasRef.current.getContext("2d");
+
+      // get the user's video/webcam stream then 1) set the video source to the stream and 2) send the stream to MediaPipe
+      const constraints = {
+        video: { width: { min: 1280 }, height: { min: 720 } },
+      };
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        if (inputVideoRef.current) {
+          inputVideoRef.current.srcObject = stream;
+        }
+        sendToMediaPipe();
+      });
+
+      // set up MediaPipe hands
+      const hands = new Hands({
+        locateFile: (file) =>
+          `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${VERSION}/${file}`,
+      });
+
+      // set up MediaPipe hands options. See https://google.github.io/mediapipe/solutions/hands.html#configuration-options
+      hands.setOptions({
+        maxNumHands: 2,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+      });
+
+      // set up MediaPipe hands callbacks. When media pipe finishes processing the video, it will call the onResults function
+      // that is where we will draw the hand landmarks and connections
+      hands.onResults(onResults);
+
+      // function that continuously sends the video stream to MediaPipe
+      const sendToMediaPipe = async () => {
+        if (inputVideoRef.current) {
+          if (!inputVideoRef.current.videoWidth) {
+            requestAnimationFrame(sendToMediaPipe);
+          } else {
+            await hands.send({ image: inputVideoRef.current });
+            requestAnimationFrame(sendToMediaPipe);
+          }
+        }
+      };
+
+      // remove the event listener when the component is unmounted
+      return () => {
+        hands.onResults(() => {});
+        hands.close();
+      };
+    }
+  }, [inputVideoReady, apiEndpoint]); // needs apiEndpoint to be in dependency array so that useEffect is called when apiEndpoint changes and the onResults is updated
+
+  /**
+   * Callback for when MediaPipe finishes processing the video stream and has some results for us.
+   * @param {results} results
+   */
+  const onResults = (results) => {
+
+    // send hand data to API endpoint if at least one hand is detected
+    if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
+      
+      // remove image data from results
+      const resultsNoImage = { ...results };
+      delete resultsNoImage.image;
+
+      // send hand data to API endpoint
+      fetch(apiEndpoint, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body:  JSON.stringify({test: "me!"}),
+        body: JSON.stringify(resultsNoImage),
+
+      }).then((response) => {
+        // successfully communicated with API endpoint
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.indexOf("application/json") !== -1) {
+          
+          // read async JSON response by creating async function and calling it
+          (async () => {
+            const json = await response.json();
+            console.log(json);
+          })();
+
+        } else {
+          console.log("API endpoint did not return JSON");
+        }
+
+      }).catch((error) => {
+        // error communicating with API endpoint
+        console.log(error);
       });
-      const json = await res.json();
-      console.log(json);
+
     }
-    fetchData();
-  }
+
+
+
+    // wait until canvas is loaded (context is 2d canvas)
+    if (canvasRef.current && contextRef.current) {
+      setLoaded(true);
+
+      // clear canvas
+      contextRef.current.save();
+      contextRef.current.clearRect(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      contextRef.current.drawImage(
+        results.image,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+
+      // draw hands
+      if (results.multiHandLandmarks && results.multiHandedness) {
+        // iterate through each hand
+        for (
+          let index = 0;
+          index < results.multiHandLandmarks.length;
+          index++
+        ) {
+          // draw the hand landmarks and connections
+          const classification = results.multiHandedness[index];
+          const isRightHand = classification.label === "Right";
+          const landmarks = results.multiHandLandmarks[index];
+          drawConnectors(contextRef.current, landmarks, HAND_CONNECTIONS, {
+            color: isRightHand ? "#00FF00" : "#FF0000",
+          });
+          drawLandmarks(contextRef.current, landmarks, {
+            color: isRightHand ? "#00FF00" : "#FF0000",
+            fillColor: isRightHand ? "#FF0000" : "#00FF00",
+            radius: (data) => {
+              return lerp(data.from.z, -0.15, 0.1, 10, 1);
+            },
+          });
+        }
+      }
+
+      // restore canvas
+      contextRef.current.restore();
+    }
+  };
 
   return (
-    <div>
-      <h1>Show me the hands!</h1>
-      <button onClick={onButtonPress}>Click me!</button>
+    <div className={styles.handsContainer}>
+      <video
+        autoPlay
+        ref={(el) => {
+          inputVideoRef.current = el;
+          setInputVideoReady(!!el);
+        }}
+        className={styles.inputVideo}
+      />
+      <div className={styles.canvasContainer}>
+        <canvas
+          className={styles.outputCanvas}
+          ref={canvasRef}
+          width={canvasWidth}
+          height={canvasHeight}
+        />
+      </div>
     </div>
   );
 }
--- pages/handtest.js
+++ pages/handtest.js
@@ -4,13 +4,48 @@
  */
 
 import HandTracker from "../components/handtracker";
+import styles from "./handtest.module.scss";
+import { useState } from "react";
 
 const handtest = () => {
-    return (
-        <div>
-            <HandTracker />
-        </div>
-    )
-}
+  const [apiEndpoint, setApiEndpoint] = useState("http://localhost:5000");
+
+  // just using this so every keystroke doesn't send a request to the MediaPipe module
+  const [apiEndpointToSend, setApiEndpointToSend] = useState(
+    "http://localhost:5000"
+  );
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.endpointContainer}>
+        <label htmlFor="apiEndpoint" >Backend API Endpoint: </label>
+        <input
+          type="text"
+          id="apiEndpoint"
+          value={apiEndpoint}
+          onChange={(e) => {
+            setApiEndpoint(e.target.value);
+          }}
+          width="100%"
+        />
+        <button
+          onClick={() => {
+            setApiEndpointToSend(apiEndpoint);
+          }}
+        >
+          Set API Endpoint
+        </button>
+
+      </div>
+      <div className={styles.handTrackerContainer}>
+        <HandTracker
+          canvasWidth={1280 / 2}
+          canvasHeight={720 / 2}
+          apiEndpoint={apiEndpointToSend}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default handtest;
